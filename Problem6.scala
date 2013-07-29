a90adbfa5b415f699fc5f59271a2efc0c4402dc3
Adas <adasurbonas@gmail.com>
2013-07-29 19:42:26 +0300

    Problem 6
    Problem description:
    The sum of the squares of the first ten natural numbers is,
    
    12 + 22 + ... + 102 = 385
    The square of the sum of the first ten natural numbers is,
    
    (1 + 2 + ... + 10)^2 = 552 = 3025
    Hence the difference between the sum of the squares of the first ten
    natural numbers and the square of the sum is 3025 − 385 = 2640.
    
    Find the difference between the sum of the squares of the first one
    hundred natural numbers and the square of the sum.

---
+++ Problem6.scala
@@ -0,0 +1,24 @@
+/* Problem description:
+* The sum of the squares of the first ten natural numbers is,
+*
+*  12 + 22 + ... + 102 = 385
+* The square of the sum of the first ten natural numbers is,
+*
+* (1 + 2 + ... + 10)^2 = 552 = 3025
+* Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+* 
+* Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+package Problem6
+
+object Problem6 {
+def main(args: Array[String]){
+	var Suma = (1 to 100).foldLeft(0)(_ + _);
+	var SquareSuma = Suma * Suma
+	var SumaSquare: Int = 0
+	
+	(1 to 100).foreach(n => SumaSquare += n * n)
+	println("Answer: "+ (SquareSuma - SumaSquare))
+	}
+}
